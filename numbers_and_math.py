e9a53635bfeda76d28f6cad01ad461e88c8b9187
Joel Jossie <joeljossie@gmail.com>
2022-03-11 13:48:48 -0700

    fixed the tile collision problem  It was just that caption() was being called twice, in on_draw() and on_update()

--- numbers_and_math.py
+++ numbers_and_math.py
@@ -1,9 +1,6 @@
-import imp
 import arcade
 import random
 import operator
-
-from numpy import isin
 
 from constant import *
 
@@ -13,12 +10,15 @@ class NumberBlock(arcade.Sprite):
     A sprite that draws itself as a crate with its stored value as a number on top.
     """
 
-    def __init__(self, value=0):
+    def __init__(self, sprite_list, value):
         super().__init__()
+        assert (value is not None and sprite_list is not None)
         self.value = value
         self.texture = arcade.load_texture(CRATE_BLUE_PATH)
         self.scale = TILE_SCALING
-        # self.draw()
+
+        # Add myself to a sprite list
+        sprite_list.append(self)
 
     def update_animation(self, delta_time: float = 1 / 60):
         # Draw this block's numeric value on top of this sprite.
@@ -44,23 +44,30 @@ class NumberBlock(arcade.Sprite):
         self.center_x = x
         self.center_y = y
 
+    def __str__(self):
+        return super.__str__(self) + f"\nNumberBlock Val: {self.value} \nSpriteList: {self.sprite_lists}\n\n"
+
 
 class NumberBlockGroup:
     """
     One or more (probably up to 3) NumberBlocks that represent a single value.
     """
 
-    def __init__(self, x=0, y=0, blocks=None, from_number=None):
+    def __init__(self, sprite_list=None, x=0, y=0, blocks=None, from_number=None):
+        assert (sprite_list is not None)
+        self.sprite_list = sprite_list
+        self.center_x = x
+        self.center_y = y
         if from_number is None:
             self._blocks = blocks
             self.value = self._compute_value()
         else:
             assert (blocks is None)
-            self.value = int(from_number)
+            try:
+                self.value = int(from_number)
+            except ValueError:
+                self.value = from_number
             self._blocks = self._make_blocks_from_number()
-        self.center_x = x
-        self.center_y = y
-        self.sprite_list = None
 
     def _compute_value(self):
         value = 0
@@ -71,16 +78,20 @@ class NumberBlockGroup:
         return value
 
     def _make_blocks_from_number(self):
-        finished = False
-        multiplier = 1
-        temp_val = self.value
         blocks = []
-        while not finished:
-            single_digit = int(((temp_val % (multiplier * 10)) - (temp_val % multiplier)) / multiplier)
-            blocks.insert(0, NumberBlock(single_digit))
-            multiplier *= 10
-            if temp_val // multiplier == 0:
-                finished = True
+        temp_val = self.value
+
+        if isinstance(temp_val, str):
+            blocks.append(NumberBlock(self.sprite_list, temp_val))
+        else:
+            finished = False
+            multiplier = 1
+            while not finished:
+                single_digit = int(((temp_val % (multiplier * 10)) - (temp_val % multiplier)) / multiplier)
+                blocks.insert(0, NumberBlock(self.sprite_list, single_digit))
+                multiplier *= 10
+                if temp_val // multiplier == 0:
+                    finished = True
 
         return blocks
 
@@ -95,27 +106,22 @@ class NumberBlockGroup:
     def _update_value(self):
         self.value = self._compute_value()
 
-    def draw(self, sprite_list=None):
+    def draw(self):
         """
+        UPDATE: DEPRECATED I THINK
         Just to clarify, this is not overriding a built-in arcade draw() function.
         It just tells each NumberBlock in the list where to place itself and which texture to use.
         """
+        assert(False)
         # Checks to ensure we already have a sprite_list for this object.
-        if sprite_list is None:
-            assert(self.sprite_list is not None)
-        # Stores a sprite_list into this object the first time it's called.
-        else:
-            self.sprite_list = sprite_list
-
         for block in self._blocks:
             self.sprite_list.append(block)
 
     def _update_locations(self):
         for index, block in enumerate(self._blocks):
-            offset =  index * TILE_SIZE * TILE_SCALING * 2
+            offset = index * TILE_SIZE * TILE_SCALING * 2
             block.center_x = self.center_x + offset
             block.center_y = self.center_y
-            print(f"block {block.value}: ({block.center_x}, {block.center_y}) - offset: {offset}")
 
     def move_to(self, x, y):
         """
@@ -129,6 +135,10 @@ class NumberBlockGroup:
 
     def get_size(self):
         return len(self._blocks)
+
+    def log(self):
+        for block in self._blocks:
+            print(str(block))
 
 
 class SimpleMathProblem:
@@ -172,6 +182,7 @@ def get_clean_problem(min=None, max=None):
         if isinstance(prob.answer, int):
             valid = True
         elif isinstance(prob.answer, float) and prob.answer.is_integer():
+            prob.answer = int(prob.answer)
             valid = True
 
     return prob
@@ -188,34 +199,27 @@ class VisualMathProblem:
         self.center_x = center_x
         self.center_y = center_y
 
+        self.sprite_list = self.scene.get_sprite_list(LAYER_NAME_NUMBER)
         self.problem = get_clean_problem(min, max)
-        self.lhs = NumberBlockGroup(self.center_x, self.center_y, from_number=self.problem.lhs)
-        self.rhs = NumberBlockGroup(self.center_x + (4 * TILE_SIZE), self.center_y, from_number=self.problem.rhs)
-        self.answer = NumberBlockGroup(self.center_x + (8 * TILE_SIZE), self.center_y,
-                                       from_number=int(self.problem.answer))
-        self.equals = NumberBlock("=")
-        self.operator = NumberBlock(str(self.problem.operator))
+        self.lhs = NumberBlockGroup(sprite_list=self.sprite_list, from_number=self.problem.lhs)
+        self.operator = NumberBlockGroup(sprite_list=self.sprite_list, from_number=str(self.problem.operator))
+        self.rhs = NumberBlockGroup(sprite_list=self.sprite_list, from_number=self.problem.rhs)
+        self.equals = NumberBlockGroup(sprite_list=self.sprite_list, from_number="=")
+        self.answer = NumberBlockGroup(sprite_list=self.sprite_list, from_number=self.problem.answer)
 
         self.draw_order = [self.lhs, self.operator, self.rhs, self.equals, self.answer]
 
     def draw(self):
-        sprite_list = self.scene.get_sprite_list(LAYER_NAME_NUMBER)
-        self.lhs.draw(sprite_list)
-        self.rhs.draw(sprite_list)
-        self.answer.draw(sprite_list)
-        sprite_list.append(self.equals)
-        sprite_list.append(self.operator)
-
         x = self.center_x
         y = self.center_y
         space = TILE_SIZE * TILE_SCALING * 2
         for chunk in self.draw_order:
-            size = 1
-            if isinstance(chunk, NumberBlockGroup):
-                size = chunk.get_size()
+            size = chunk.get_size()
             chunk.move_to(x, y)
 
             # Move over to the next space
             x += space * size + space
 
-            print(f"chunk {chunk.value}: ({chunk.center_x}, {chunk.center_y})")
+    def log(self):
+        for block in self.draw_order:
+            block.log()
